0865c4d695f7517910c0a93ffa645c5e4d38b568
avedado <mariaalonsoteis@gmail.com>
2025-05-15 13:55:18 +0200

    test: Introducir primer caso test

--- src/test/java/edu/estatuas/AppTest.java
+++ src/test/java/edu/estatuas/AppTest.java
@@ -1,38 +1,37 @@
 package edu.estatuas;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    public RomanNumber numeroRomano;
 
     /**
-     * @return the suite of tests being tested
+     * Grupos sumatorios M, C, X, I
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+
+    @ParameterizedTest
+    @CsvSource({
+            "1000,  M",
+            "2000,  UMMU",
+            "3000,  UMMMU"
+    })
+    @Tag("sumatorio")
+    public void grupo_M_test(Short decimal, String roman) {
+
+        //String testCase = "M";
+        numeroRomano = new RomanNumber(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+
+        /**
+         * El caso MMMM es control de errores
+         * y no puede estar en el test de la logica
+         * Asumimos que la entrada es correcta.
+         * Sino, hay que programar la gestion de errores
+         */
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
