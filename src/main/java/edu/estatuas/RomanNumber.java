dfeda4baf9c48f4f7be1183ed46c77288aa3db68
avedado <mariaalonsoteis@gmail.com>
2025-05-15 13:55:46 +0200

    feat: Crear la clase RomanNumber

---
+++ src/main/java/edu/estatuas/RomanNumber.java
@@ -0,0 +1,16 @@
+package edu.estatuas;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RomanNumber {
+    private String romanNum;
+    private int decimalNum;
+
+    public Double toDecimal() {
+        for(String expresion : )
+    }
+
+
+
+}
