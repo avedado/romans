136f3ed73baf74ba5c310760b08ca862302ed4dc
avedado <mariaalonsoteis@gmail.com>
2025-05-15 13:48:36 +0200

    feat: Inctroducir la expresion que deben cumplir los numeros romanos

---
+++ src/main/java/edu/estatuas/RegexRomanNumbers.java
@@ -0,0 +1,14 @@
+package edu.estatuas;
+
+public class RegexRomanNumbers {
+
+    private String expresion="([I|X|M|C]{0,3}|[LDV])";
+
+    public String getExpression(String expresion){
+        return this.expresion;
+    }
+
+
+
+
+}
