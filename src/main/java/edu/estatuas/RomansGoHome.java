56985799572dafcbb15f1f076870fc9717237d1c
María Alonso <mariaalonsoteis@gmail.com>
2025-06-22 22:23:43 +0200

    Update RomansGoHome.java

--- src/main/java/edu/estatuas/RomansGoHome.java
+++ src/main/java/edu/estatuas/RomansGoHome.java
@@ -1,5 +1,19 @@
 package edu.estatuas;
 
 public class RomansGoHome{
+  public static void main(String[] args) {
+
+        List<String> romanNumbers = List.of(
+                "MMMDCCCLXXXVIII", // 3888
+                "MMDCCLXXVII",  // 2777
+                "CDXLIV", // 444
+                "CDXXXIX" // 439
+        );
+
+        for (String roman : romanNumbers) {
+            RomanNumber number = new RomanNumber(roman);
+            System.out.println(number.toString() + " = " + number.toDecimal());
+        }
+    }
 
 }
